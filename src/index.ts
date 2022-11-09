1c6a900402d8bb204148ebdef4ee76538fa8b9ac
Guilherme Riguitti <71913845+GuilhermeRiguitti@users.noreply.github.com>
2022-11-09 13:50:29 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,28 @@ function tipoVariavel(variavel: any) : void {
 }
 
 tipoVariavel(10)
+
+enum GENERO {
+    ACAO="ação",
+    DRAMA="drama",
+    COMEDIA="comédia",
+    ROMANCE="romance",
+    TERROR="terror"
+    }
+            
+type filme = { 
+    nome: string, 
+    lancamento: string, 
+    genero: GENERO, 
+    avaliacao: number | null
+} 
+
+const filme1: filme = {
+    nome: "dev",
+    lancamento: "10/04/2020",
+    genero: GENERO.ACAO,
+    avaliacao: 10
+}
+
+console.log(filme1)
+
